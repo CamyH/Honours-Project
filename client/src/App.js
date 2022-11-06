f106a161ce0081b7acd4d340e7aa62f6fc43e43b
Cameron <cameronhunt88@hotmail.com>
2022-11-06 12:37:34 +0000

    added scss and router packages + bg color

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MainMenu from "./components/main-menu/MainMenu";
+import "./App.scss";
 
 function App() {
-  return (
-  
-  );
+	return (
+		<Router basename={process.env.PUBLIC_URL}>
+			<Routes>
+				<Route path="/" element={<MainMenu />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
---
+++ client/src/components/main-menu/MainMenu.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MainMenu() {
+	return (
+		<React.Fragment>
+			<h1>Test</h1>
+		</React.Fragment>
+	);
+}
+
+export default MainMenu;
