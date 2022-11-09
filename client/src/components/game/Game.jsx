9a3fc528a6601ef65a81b7b1ad0e20a3c9fc115e
CamyH <cameronhunt88@hotmail.com>
2022-11-09 15:16:24 +0000

    game page setup and link added to button

---
+++ client/src/components/game/Game.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./game.scss";
+import { Link } from 'react-router-dom';
+
+function Game() {
+    return (
+        <React.Fragment>
+            <h1>Game Page</h1>
+        </React.Fragment>
+    );
+}
+
+export default Game;
--- client/src/components/main-menu/MainMenu.jsx
+++ client/src/components/main-menu/MainMenu.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import "./main-menu.scss"
+
 
 function MainMenu() {
 	return (
 		<React.Fragment>
 			<section id="main-menu-wrapper">
 				<h1 id="main-menu-header">SQL Joins Game</h1>
-				<button className="btn btn-rect-to-round btn-rect-to-round--black">Play Game</button>
+				<Link to={"/game"}>
+					<button className="btn btn-rect-to-round btn-rect-to-round--black">Play Game</button>
+				</Link>
 			</section>
 		</React.Fragment>
 	);
