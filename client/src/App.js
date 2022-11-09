7bc4bda098435d071f974670d4150b8bcfc2eb9f
CamyH <cameronhunt88@hotmail.com>
2022-11-09 15:13:24 +0000

    added route for game component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MainMenu from "./components/main-menu/MainMenu";
+import Game from "./components/game/Game";
 import "./App.scss";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
 		<Router basename={process.env.PUBLIC_URL}>
 			<Routes>
 				<Route path="/" element={<MainMenu />} />
+				<Route path="/game" element={<Game />} />
 			</Routes>
 		</Router>
 	);
