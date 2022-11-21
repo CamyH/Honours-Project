4c70b11d8f0d35924c891ab41812461ce3d41fb5
CamyH <cameronhunt88@hotmail.com>
2022-11-21 16:31:25 +0000

    added html for score and question number

--- client/src/components/game/Game.jsx
+++ client/src/components/game/Game.jsx
@@ -5,7 +5,9 @@ import { Link } from 'react-router-dom';
 function Game() {
     return (
         <React.Fragment>
+            <h2 className='question-num'>{}</h2>
             <h1 id='game-page-header'>Learn SQL Joins</h1>
+            <h2 className='score'>{}</h2>
         </React.Fragment>
     );
 }
