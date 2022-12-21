36242ed113bfacbece5c94538050fc762fe490d2
CamyH <cameronhunt88@hotmail.com>
2022-12-21 10:59:35 +0000

    added setup for displaying questions

--- client/src/components/game/Game.jsx
+++ client/src/components/game/Game.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./game.scss";
 import { Link } from 'react-router-dom';
+import DisplayQuestion from '../questions/DisplayQuestions';
 
 function Game() {
     return (
@@ -8,6 +9,8 @@ function Game() {
             <h2 className='question-num'>{}</h2>
             <h1 id='game-page-header'>Learn SQL Joins</h1>
             <h2 className='score'>{}</h2>
+
+            <DisplayQuestion />
         </React.Fragment>
     );
 }
---
+++ client/src/components/questions/DisplayQuestions.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./questions.scss";
+import { Link } from 'react-router-dom';
+
+function DisplayQuestions() {
+    return (
+        <React.Fragment>
+            
+        </React.Fragment>
+    );
+}
+
+export default DisplayQuestions;
