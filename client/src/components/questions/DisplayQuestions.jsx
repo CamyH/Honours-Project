83c81db7639d4939d4866b666cda11fd546bc849
CamyH <cameronhunt88@hotmail.com>
2023-01-05 10:58:49 +0000

    added component to display questions

--- client/src/components/questions/DisplayQuestions.jsx
+++ client/src/components/questions/DisplayQuestions.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import "./questions.scss";
+import Questions from "../../assets/Questions.json";
 import { Link } from 'react-router-dom';
 
 function DisplayQuestions() {
     return (
-        <React.Fragment>
-            
-        </React.Fragment>
+        <div className='questions-container'>
+        {Questions.slice(0,3).map((item, index) => {
+            return <h1 key={index} className='questions'>{item.Content}</h1>
+        })}
+        </div>
     );
 }
 
