2d5fe9bb92f2f0c7a7f9ff7c63a3b293cd9a583e
CamyH <cameronhunt88@hotmail.com>
2023-01-08 11:16:40 +0000

    questions display + question counter added

--- client/src/components/questions/DisplayQuestions.jsx
+++ client/src/components/questions/DisplayQuestions.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import { React, useState } from 'react'
 import "./questions.scss";
 import Questions from "../../assets/Questions.json";
 import { Link } from 'react-router-dom';
 
 function DisplayQuestions() {
+    // Counter starts at 1 for user readability
+    const [questionCount, setQuestionCount] = useState(1)
+
     return (
-        <div className='questions-container'>
-        {Questions.slice(0,3).map((item, index) => {
-            return <h1 key={index} className='questions'>{item.Content}</h1>
-        })}
+        <div className='content-container'>
+            <h3 className='question-counter'>Question: {questionCount}/13</h3>
+            <div className='questions-container'>
+                <h1 className='questions'>{Questions[questionCount-1].Content}</h1>
+            </div>
+            <button className='next-question-btn' type='submit' onClick={() => setQuestionCount(questionCount+1)}>Next</button>
         </div>
     );
 }
